324e488677b40fa4121919a6ebad081f68b548e0
Richard Jacobsen <rjacobsen2009@gmail.com>
2019-08-02 23:30:28 -0400

    final commit - code cleanup - remove uneeded files

--- setup/pre-provision.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-echo "Installing vagrant-vbguest..."
-vagrant plugin install vagrant-vbguest
-vagrant plugin install vagrant-disksize
-vagrant plugin install vagrant-cachier
-vagrant plugin install vagrant-faster
